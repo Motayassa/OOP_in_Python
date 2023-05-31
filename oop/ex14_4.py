cd443eb107988b514d7ed29b4b5f583f74b4d2a6
Motayassa <malovilyajob@gmail.com>
2023-05-31 16:32:30 +0300

    Update ex14_4.py

--- oop/ex14_4.py
+++ oop/ex14_4.py
@@ -1,31 +1,24 @@
 class Translator:
-    '''для перевода с английского на русский''' 
+    '''для перевода с английского на русский'''
     def add(self, eng, rus):
-        '''- для добавления новой связки английского и русского слова (если английское слово уже существует,
-        то новое русское слово добавляется как синоним для перевода, например, go - идти, ходить, ехать);
-        если связка eng-rus уже существует, то второй раз ее добавлять не нужно, 
-        например:  add('go', 'идти'), add('go', 'идти')'''
         if 'tr' not in self.__dict__:
             self.tr={}
-           
         self.tr.setdefault(eng,[])
         if rus not in self.tr[eng]:
             self.tr[eng].append(rus)
-        
-
+            
     def remove(self, eng):
         '''- для удаления связки по указанному английскому слову'''
         del self.tr[eng]
-    
-    
+        
     def translate(self, eng): 
         '''- для перевода с английского на русский (метод должен возвращать список из русских слов,
         соответствующих переводу английского слова, даже если в списке всего одно слово)'''
         return self.tr[eng]
 
 
-tr=Translator()
-line_in='''tree - дерево
+tr = Translator()
+line_in = '''tree - дерево
 car - машина
 car - автомобиль
 leaf - лист
@@ -40,6 +33,4 @@ for i in line_in:
     tr.add(eng_word,rus_word)
 
 tr.remove('car')
-print(*tr.translate('go'))
-
-
+print ( * tr.translate ( 'go' ) )
