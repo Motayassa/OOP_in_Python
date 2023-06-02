bccf8fdf4c7c92253de111e60e47f75b6e3f72d6
Motayassa <malovilyajob@gmail.com>
2023-06-02 15:27:56 +0300

    Add random point coordinates and random shapes

--- oop/init_and_del/ex1_6.py
+++ oop/init_and_del/ex1_6.py
@@ -1,3 +1,6 @@
+import random
+
+
 class Line:
     def __init__(self, a, b, c, d):
         self.sp = (a, b)
@@ -14,3 +17,14 @@ class Ellipse:
     def __init__(self, a, b, c, d):
         self.sp = (a, b)
         self.ep = (c, d)
+
+
+LIST_CLASSES = [Line, Rect, Ellipse]
+randcl = random.choice(LIST_CLASSES)
+elements = [Line(0, 0, 0, 0)
+            if randcl == Line
+            else randcl(a=random.randrange(-100, 100),
+                        b=random.randrange(-100, 100),
+                        c=random.randrange(-100, 100),
+                        d=random.randrange(-100, 100))
+            for i in range(217)]
