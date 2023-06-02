c5950f8aab5d18361812f9cfccb37124ead1f9e9
Motayassa <malovilyajob@gmail.com>
2023-06-02 20:41:41 +0300

    Fix bugs in OOP

--- oop/init_and_del/ex1_7.py
+++ oop/init_and_del/ex1_7.py
@@ -6,34 +6,34 @@ class TriangleChecker:
 
     def is_triangle(self, a, b, c):
         if (
-            type(a) != 'int' or type(a) != 'float' or
-            type(b) != 'int' or type(b) != 'float' or
-            type(c) != 'int' or type(c) != 'float'
+            (type(self.a) not in (int, float)) or
+            (type(self.b) not in (int, float)) or
+            (type(self.c) not in (int, float))
         ):
             return 1
 
         if (
-            a < 0 or a == 0 or
-            b < 0 or b == 0 or
-            c < 0 or c == 0
+            self.a < 0 or self.a == 0 or
+            self.b < 0 or self.b == 0 or
+            self.c < 0 or self.c == 0
         ):
             return 1
 
         if (
-            a + b <= c or
-            a + c <= b or
-            b + c <= a
+            self.a + self.b <= self.c or
+            self.a + self.c <= self.b or
+            self.b + self.c <= self.a
         ):
             return 2
 
         if (
-            a + b > c and
-            a + c > b and
-            b + c > a
+            self.a + self.b > self.c and
+            self.a + self.c > self.b and
+            self.b + self.c > self.a
         ):
             return 3
 
 
 a, b, c = map(int, input().split())
 tr = TriangleChecker(a, b, c)
-print(tr.is_triangle)
+print(tr.is_triangle(a, b, c))
