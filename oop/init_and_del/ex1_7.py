6c99fffba5aa8c4c89704c577437410b4033d1a3
Motayassa <malovilyajob@gmail.com>
2023-06-02 18:51:56 +0300

    Add creating a triangle and checking the sides of the triangle

--- oop/init_and_del/ex1_7.py
+++ oop/init_and_del/ex1_7.py
@@ -1,1 +1,37 @@
+class TriangleChecker:
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
+
+    def is_triangle(self, a, b, c):
+        if (
+            type(a) != 'int' or type(a) != 'float' or
+            type(b) != 'int' or type(b) != 'float' or
+            type(c) != 'int' or type(c) != 'float'
+        ):
+            return 1
+
+        if (
+            a < 0 or a == 0 or
+            b < 0 or b == 0 or
+            c < 0 or c == 0
+        ):
+            return 1
+
+        if (
+            a + b <= c or
+            a + c <= b or
+            b + c <= a
+        ):
+            return 2
+
+        if (
+            a + b > c and
+            a + c > b and
+            b + c > a
+        ):
+            return 3
+
+
 a, b, c = map(int, input().split())
