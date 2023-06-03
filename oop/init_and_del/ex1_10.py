1c3d138edeec445c873eb9c7fe93f4f315335a0a
Motayassa <malovilyajob@gmail.com>
2023-06-03 11:58:53 +0300

    Create finish version

--- oop/init_and_del/ex1_10.py
+++ oop/init_and_del/ex1_10.py
@@ -1,38 +1,6 @@
-'''Подвиг 8. Объявите в программе класс Cart (корзина), объекты которого создаются командой:
-
-cart = Cart()
-Каждый объект класса Cart должен иметь локальное свойство goods - список объектов для покупки (объекты классов Table, TV, Notebook и Cup). Изначально этот список должен быть пустым.
-
-В классе Cart объявить методы:
-
-add(self, gd) - добавление в корзину товара, представленного объектом gd;
-remove(self, indx) - удаление из корзины товара по индексу indx;
-get_list(self) - получение из корзины товаров в виде списка из строк:
-
-['<наименовние_1>: <цена_1>',
-'<наименовние_2>: <цена_2>',
-...
-'<наименовние_N>: <цена_N>']
-
-Объявите в программе следующие классы для описания товаров:
-
-Объекты этих классов должны создаваться командой:
-
-gd = ИмяКласса(name, price)
-Каждый объект классов товаров должен содержать локальные свойства:
-
-name - наименование;
-price - цена.
-
-Создайте в программе объект cart класса Cart. Добавьте в него два телевизора (TV), один стол (Table), два ноутбука (Notebook) и одну кружку (Cup). Названия и цены придумайте сами.
-
-P.S. Отображать на экране ничего не нужно, только создать объекты по указанным требованиям.'''
-
-
 class Cart:
-    def __init__(self, goods):
-        goods = []
-        self.goods = goods
+    def __init__(self):
+        self.goods = []
 
     def add(self, gd):
         self.goods.append(gd)
@@ -41,7 +9,7 @@ class Cart:
         self.goods.pop(indx)
 
     def get_list(self):
-        return [", ".join(map(lambda x: f"{x.name}: {x.price}", self.goods))]
+        return [f"{x.name}: {x.price}" for x in self.goods]
 
 
 def create_product(self, name, price):
@@ -50,19 +18,32 @@ def create_product(self, name, price):
 
 
 class Table:
-    create_product()
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
 
 
 class TV:
-    create_product()
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
 
 
 class Notebook:
-    create_product()
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
 
 
 class Cup:
-    create_product()
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
 
 
 cart = Cart()
+cart.add(TV('LG', 5000))
+cart.add(Table('Ikea', 343))
+cart.add(Notebook('Apple', 12000))
+cart.add(Notebook('Apple', 7800))
+cart.add(Cup('{a[', 10))
