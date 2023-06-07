cdf489f2273fb50be6e907e5f34acf4c175d1c0c
Motayassa <malovilyajob@gmail.com>
2023-06-07 17:49:10 +0300

    Finish

--- oop/init_and_del/ex1_12.py
+++ oop/init_and_del/ex1_12.py
@@ -5,17 +5,17 @@ class Cell:
     def __init__(self, around_mines=0, mine=False):
         self.around_mines = around_mines
         self.mine = mine
-        self.fl_open = False
+        self.fl_open = True
 
 
 class GamePole:
     def __init__(self, n, m):
         self._n = n
         self._m = m
-        self.pole = [[Cell() for i in range(self._n)] for i in range(self._n)]
+        self.pole = [[Cell() for n in range(self._n)] for n in range(self._n)]
         self.init()
 
-    def init(self, m):
+    def init(self):
         m = 0
         while m < self._m:
             i = randint(0, self._n - 1)
@@ -29,10 +29,7 @@ class GamePole:
         for x in range(self._n):
             for y in range(self._n):
                 if not self.pole[x][y].mine:
-                    mines = ((
-                        self.pole[x+i][y+j].mine for i, j in indx if
-                        0 <= i <= self._n and 0 <= j <= self._n
-                        ))
+                    mines = sum((self.pole[x+i][y+j].mine for i, j in indx if 0 <= x+i < self._n and 0 <= y+j < self._n))
                     self.pole[x][y].around_mines = mines
 
     def show(self):
@@ -41,5 +38,5 @@ class GamePole:
                        if not x.mine else '*', row))
 
 
-pole_game = (10, 12)
+pole_game = GamePole(10, 12)
 pole_game.show()
