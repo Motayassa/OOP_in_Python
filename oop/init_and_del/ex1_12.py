94ae62a8b32eee2321f10df18649be498cc2a936
Motayassa <malovilyajob@gmail.com>
2023-06-07 16:26:45 +0300

    Add class GamePole and init

--- oop/init_and_del/ex1_12.py
+++ oop/init_and_del/ex1_12.py
@@ -1,5 +1,26 @@
+from random import randint
+
+
 class Cell:
-    def __init__(self, mine, around_mines=0):
+    def __init__(self, around_mines=0, mine=False):
         self.around_mines = around_mines
         self.mine = mine
         self.fl_open = False
+
+
+class GamePole:
+    def __init__(self, n, m):
+        self._n = n
+        self._m = m
+        self.pole = [[Cell() for i in range(self._n)] for i in range(self._n)]
+        self.init()
+
+    def init(self, m):
+        m = 0
+        while m < self._m:
+            i = randint(0, self._n - 1)
+            j = randint(0, self._n - 1)
+            if self.pole[i][j].mine:
+                continue
+            self.pole[i][j].mine = True
+            m += 1
