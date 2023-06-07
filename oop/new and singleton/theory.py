a455c63349af3a134d8bbbea61dd532812f4a31f
Motayassa <malovilyajob@gmail.com>
2023-06-07 18:51:40 +0300

    Add content in theory.py

--- oop/new and singleton/theory.py
+++ oop/new and singleton/theory.py
@@ -1,8 +1,50 @@
 class Point:
     def __new__(cls, *args, **kwargs):
         print("вызов __new__ для " + str(cls))
- 
+        return super().__new__(cls)
+
     def __init__(self, x=0, y=0):
         print("вызов __init__ для " + str(self))
         self.x = x
         self.y = y
+
+
+pt = Point(1, 2)
+print(pt)
+
+# singleton
+
+
+class DataBase:
+    __instance = None
+
+    def __new__(cls, *args, **kwargs):  # singleton -
+        # создание нового объекта класса
+        if cls.__instance is None:
+            cls.__instance = super().__new__(cls)
+
+        return cls.__instance
+
+    def __del__(self):  # финализатор обнуляет значение атрибута класса
+        # DataBase, после удаления объекта класса
+        DataBase.__instance = None
+
+    def __init__(self, user, psw, port):
+        self.user = user
+        self.psw = psw
+        self.port = port
+
+    def connect(self):
+        print(f"соединение с БД: {self.user}, {self.psw}, {self.port}")
+
+    def close(self):
+        print("закрытие соединения с БД")
+
+    def read(self):
+        return "данные из БД"
+
+    def write(self, data):
+        print(f"запись в БД {data}")
+
+
+db = DataBase('root', '1234', 80)
