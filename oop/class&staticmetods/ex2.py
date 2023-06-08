26e41c665cff6d2caa6cb2eeb854d176c8d80e8f
Motayassa <malovilyajob@gmail.com>
2023-06-08 19:12:30 +0300

    Add class FormLogin

---
+++ oop/class&staticmetods/ex2.py
@@ -0,0 +1,14 @@
+class FormLogin:
+    def __init__(self, lgn, psw):
+        self.login = lgn
+        self.password = psw
+
+    def render_template(self):
+        return "\n".join(['<form action="#">',
+                          self.login.get_html(),
+                          self.password.get_html(),
+                          '</form>'])
+
+
+login = FormLogin(TextInput("Логин"), PasswordInput("Пароль"))
+html = login.render_template()
