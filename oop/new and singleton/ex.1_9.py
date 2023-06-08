419af9451860778ac3abccb8d065c93071e98c92
Motayassa <malovilyajob@gmail.com>
2023-06-08 13:22:01 +0300

    Add method __new__ in class Point

--- oop/new and singleton/ex.1_9.py
+++ oop/new and singleton/ex.1_9.py
@@ -1,4 +1,10 @@
 class Point:
+    __instance = None
+
+    def __new__(cls, *args, **kwargs):
+        cls.__instance = super().__new__(cls)
+        return cls.__instance
+
     def __init__(self, x=0, y=0):
         self.x = x
         self.y = y
