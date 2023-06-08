3dc246e67a7add7df5b144b0f5515e0ab3986a98
Motayassa <malovilyajob@gmail.com>
2023-06-08 13:04:00 +0300

    Finish ex.1_8.py

--- oop/new and singleton/ex.1_8.py
+++ oop/new and singleton/ex.1_8.py
@@ -1,4 +1,5 @@
-TYPE_OS = 1 # 1 - Windows; 2 - Linux
+TYPE_OS = 1  # 1 - Windows; 2 - Linux
+
 
 class DialogWindows:
     name_class = "DialogWindows"
@@ -6,3 +7,22 @@ class DialogWindows:
 
 class DialogLinux:
     name_class = "DialogLinux"
+
+
+class Dialog:
+    def __new__(cls, *args, **kwargs):
+        obj = None
+        if TYPE_OS == 1:
+            obj = super().__new__(DialogWindows)
+        else:
+            obj = super().__new__(DialogLinux)
+
+        obj.name = args[0]
+        return obj
+
+    def __init__(self, name):
+        self.name = name
+
+
+dlg = Dialog('name')
+print(dlg)
