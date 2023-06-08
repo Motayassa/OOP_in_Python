89a93699f5d6de3bea18b33a79dfc87cfa847445
Motayassa <malovilyajob@gmail.com>
2023-06-08 14:20:33 +0300

    Finish ex.1_9.py

--- oop/new and singleton/ex.1_9.py
+++ oop/new and singleton/ex.1_9.py
@@ -1,16 +1,11 @@
 class Point:
-    __instance = None
-
-    def __new__(cls, *args, **kwargs):
-        cls.__instance = super().__new__(cls)
-        return cls.__instance
-
-    def __init__(self, x=0, y=0):
+    def __init__(self, x, y):
         self.x = x
         self.y = y
 
     def clone(self):
-        pass
+        return Point(self.x, self.y)
 
 
 pt = Point(12, 7)
+pt_clone = pt.clone()
