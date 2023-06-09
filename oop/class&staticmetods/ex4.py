419eebbda16eed64e8013841f1e96cc1d1f0dc26
Motayassa <malovilyajob@gmail.com>
2023-06-09 19:18:21 +0300

    Add class YouTube

--- oop/class&staticmetods/ex4.py
+++ oop/class&staticmetods/ex4.py
@@ -4,3 +4,15 @@ class Video:
 
     def play(self):
         print(f"воспроизведение видео {self.name}")
+
+
+class YouTube:
+    videos = []
+
+    @classmethod
+    def add_video(cls, video):
+        cls.videos.append(video)
+
+    @classmethod
+    def play(cls, video_indx):
+        Video.play(cls.videos[video_indx])
