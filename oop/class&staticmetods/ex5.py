f45703234003f36b1613ff8a6142fd4f29b7a516
Motayassa <malovilyajob@gmail.com>
2023-06-09 19:52:51 +0300

    Add base structure

---
+++ oop/class&staticmetods/ex5.py
@@ -0,0 +1,20 @@
+Объявите класс AppStore - интернет-магазин приложений для устройств под iOS.
+В этом классе должны быть реализованы следующие методы:
+
+add_application(self, app) - добавление нового приложения app в магазин;
+remove_application(self, app) - удаление приложения app из магазина;
+block_application(self, app) - блокировка приложения app 
+                                (устанавливает локальное свойство blocked объекта app в значение True);
+total_apps(self) - возвращает общее число приложений в магазине.
+class AppStore:
+    def dd_application(self, app):
+        pass
+
+    def remove_application(self, app):
+        pass
+
+    def block_application(self, app):
+        pass
+
+    def total_apps(self):
+        pass
