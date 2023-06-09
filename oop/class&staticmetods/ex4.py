86e8b29e28f022feab576baab586012a38f4ceb8
Motayassa <malovilyajob@gmail.com>
2023-06-09 19:23:40 +0300

    Add tests

--- oop/class&staticmetods/ex4.py
+++ oop/class&staticmetods/ex4.py
@@ -16,3 +16,13 @@ class YouTube:
     @classmethod
     def play(cls, video_indx):
         Video.play(cls.videos[video_indx])
+
+
+v1 = Video()
+v2 = Video()
+v1.create('Python')
+v2.create('Python ООП')
+YouTube.add_video(v1)
+YouTube.add_video(v2)
+YouTube.play(0)
+YouTube.play(1)
