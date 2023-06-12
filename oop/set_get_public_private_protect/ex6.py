cf0de54f9cd87584ac79730a8b381d175b449e81
Motayassa <malovilyajob@gmail.com>
2023-06-12 18:00:56 +0300

    Finish ex6.py

--- oop/set_get_public_private_protect/ex6.py
+++ oop/set_get_public_private_protect/ex6.py
@@ -1,42 +1,41 @@
-'''Объекты класса ObjList должны иметь следующий набор приватных локальных свойств:
-
-__next - ссылка на следующий объект связного списка (если следующего объекта нет, то __next = None);
-__prev - ссылка на предыдущий объект связного списка (если предыдущего объекта нет, то __prev = None);
-__data - строка с данными.
-'''
-'''Для этого объявите класс LinkedList, который будет представлять связный список в целом и 
-иметь набор следующих методов:
-
-add_obj(self, obj) - добавление нового объекта obj класса ObjList в конец связного списка;
-remove_obj(self) - удаление последнего объекта из связного списка;
-get_data(self) - получение списка из строк локального свойства __data всех объектов связного списка.
-
-И в каждом объекте этого класса должны создаваться локальные публичные атрибуты:
-
-head - ссылка на первый объект связного списка (если список пустой, то head = None);
-tail - ссылка на последний объект связного списка (если список пустой, то tail = None).'''
-
-
 class LinkedList:
-    def __init__(self, head, tail):
-        self.head = head
-        self.tail = tail
+    def __init__(self):
+        self.head = None
+        self.tail = None
 
     def add_obj(self, obj):
-        pass
+        if self.tail:
+            self.tail.set_next(obj)
+        obj.set_prev(self.tail)
+        self.tail = obj
+        if not self.head:
+            self.head = obj
 
     def remove_obj(self):
-        pass
+        if self.tail is None:
+            return
+
+        prev = self.tail.get_prev()
+        if prev:
+            prev.set_next(None)
+        self.tail = prev
+        if self.tail is None:
+            self.head = None
 
     def get_data(self):
-        pass
+        s = []
+        h = self.head
+        while h:
+            s.append(h.get_data())
+            h = h.get_next()
+        return s
 
 
 class ObjList:
-    def __init__(self, next, prev, data):
-        self.__next = next
-        self.__prev = prev
+    def __init__(self, data):
         self.__data = data
+        self.__next = None
+        self.__prev = None
 
     def set_next(self, obj):
         self.__next = obj
@@ -48,7 +47,7 @@ class ObjList:
         return self.__next
 
     def get_prev(self):
-        return self.__next
+        return self.__prev
 
     def set_data(self, data):
         self.__data = data
