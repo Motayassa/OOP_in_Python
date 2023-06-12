cb24f9775ea6500078c84eff3928f3d6e9db6767
Motayassa <malovilyajob@gmail.com>
2023-06-12 15:53:28 +0300

    Optimizate functions

--- oop/set_get_public_private_protect/ex4.py
+++ oop/set_get_public_private_protect/ex4.py
@@ -1,9 +1,6 @@
 class Line:
     def __init__(self, x1, y1, x2, y2):
-        self.__x1 = x1
-        self.__y1 = y1
-        self.__x2 = x2
-        self.__y2 = y2
+        self.set_coords(x1, y1, x2, y2)
 
     def set_coords(self, x1, y1, x2, y2):
         if self.__check_coord(x1, y1, x2, y2):
@@ -13,10 +10,10 @@ class Line:
             self.__y2 = y2
 
     def get_coords(self):
-        return (self.__x1, self.__y1, self.__x2, self.__y2)
+        return self.__x1, self.__y1, self.__x2, self.__y2
 
     def draw(self):
-        print(self.__x1, self.__y1, self.__x2, self.__y2, sep=' ')
+        print(*self.get_coords())
 
     @classmethod
     def __check_coord(cls, x1, y1, x2, y2):
