271a14c7658d04fcbe7e52b80fb9dfa87ed5c89e
Motayassa <malovilyajob@gmail.com>
2023-06-12 16:30:24 +0300

    Finish my version

--- oop/set_get_public_private_protect/ex5.py
+++ oop/set_get_public_private_protect/ex5.py
@@ -1,26 +1,3 @@
-'''Объекты второго класса Rectangle (прямоугольник) должны создаваться командами:
-
-r1 = Rectangle(Point(x1, y1), Point(x2, y2))
-или
-
-r2 = Rectangle(x1, y1, x2, y2)
-Здесь первая координата (x1, y1) - верхний левый угол, а вторая координата (x2, y2) - правый нижний. 
-При этом, в объектах класса Rectangle (вне зависимости от способа их создания) должны формироваться 
-следующие локальные свойства:
-
-__sp - объект класса Point с координатами x1, y1 (верхний левый угол);
-__ep - объект класса Point с координатами x2, y2 (нижний правый угол).
-
-Также к классе Rectangle должны быть реализованы следующие методы:
-
-set_coords(self, sp, ep) - изменение текущих координат, где sp, ep - объекты класса Point;
-get_coords(self) - возвращение кортежа из объектов класса Point с текущими координатами прямоугольника 
-(ссылки на локальные свойства __sp и __ep);
-draw(self) - отображение в консоли сообщения: "Прямоугольник с координатами: (x1, y1) (x2, y2)". 
-Здесь x1, y1, x2, y2 - соответствующие числовые значения координат.
-
-Создайте объект rect класса Rectangle с координатами (0, 0), (20, 34).'''
-
 class Point:
     def __init__(self, x, y):
         self.__x = x
@@ -31,5 +8,27 @@ class Point:
 
     @classmethod
     def __check_coords(cls, x, y):
-        return type(x) is (int, float) and type(x) is (int, float)
+        return type(x) is (int, float) and type(y) is (int, float)
 
+
+class Rectangle:
+    def __init__(self, *args):
+        if len(args) == 2:
+            self.__sp = args[0]
+            self.__ep = args[1]
+        else:
+            self.__sp = args[0], args[1]
+            self.__ep = args[2], args[3]
+
+    def set_coords(self, sp, ep):
+        self.__sp = sp
+        self.__ep = ep
+
+    def get_coords(self):
+        return self.__sp, self.__ep
+
+    def draw(self):
+        print(f"Прямоугольник с координатами: {self.get_coords()}")
+
+
+rect = Rectangle(0, 0, 20, 34)
