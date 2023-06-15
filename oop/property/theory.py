3f6ddc24d6dadd3f0d56476ea26a669f1814bf0b
Motayassa <malovilyajob@gmail.com>
2023-06-15 10:05:29 +0300

    Finish theory

--- oop/property/theory.py
+++ oop/property/theory.py
@@ -10,24 +10,21 @@ class Person:
         self.__old = old
 
     # old = 45
-    # old = property(get_old, set_old)  альтернатива нижней  записи
+    old = property(get_old, set_old)  # альтернатива нижней  записи
     # объект property с
     # ссылкой на сеттер и геттер
     # ЭТУ СТРОЧКУ МОЖНО ПРОПИСАТЬ С ПОМОЩЬЮ ДЕКОРАТОРОВ(ниже)
+    '''
     old = property()   # строка 13 - альтернатива
     old = old.setter(set_old)
     old = old.getter(get_old)
+    '''
 
 
 p = Person('Сергей', 20)
 # p.set_old(35)
 print(p.get_old())
 print(p.old)
-print(Person.old)  # объект property
-p.old = 35
-p.__dict__['old'] = 'old in object p'
-print(p.old, p.__dict__)
-a = property()
-# a.getter()   декоратор для сеттера;
-# a.setter()   декоратор для геттера;
-# a.deleter()   декоратор для делитера.
+del p.old
+print(p.__dict__)
+p.old = 10
---
+++ oop/property/theory2.py
@@ -0,0 +1,30 @@
+# Альтерантивный код из теории 1 с использованием декораторов
+class Person:
+    def __init__(self, name, old):
+        self.__name = name
+        self.__old = old
+
+    '''
+    old = property()
+    old = old.setter(set_old)
+    old = old.getter(get_old)
+    '''
+    @property
+    def old(self):  # объект-свойство (ГЕТТЕР)
+        return self.__old
+
+    @old.setter  # СЕТТЕР
+    def get_old(self, old):  # Метод set_old нужно переименовать в get_old,
+        # чтобы имена совпадали (это обязательное условие)
+        # и перед ним прописать декоратор
+        self.__old = old
+
+    @old.deleter  # ДЕЛИТЕР
+    def old(self):
+        del self.__old
+
+
+p = Person('Сергей', 20)
+print(p.get_old)
+p.get_old = 35
+print(p.get_old)
