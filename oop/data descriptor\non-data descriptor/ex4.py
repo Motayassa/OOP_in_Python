991709c335a73f8891ae647e860e81120707ef0b
Motayassa <malovilyajob@gmail.com>
2023-06-17 18:28:40 +0300

    Finish ex4.py

--- oop/data descriptor\non-data descriptor/ex4.py
+++ oop/data descriptor\non-data descriptor/ex4.py
@@ -1,31 +1,41 @@
-'''Необходимо объявить класс Bag (рюкзак), объекты которого будут создаваться командой:
+class Bag:
+    def __init__(self, max_weight):
+        self.total_weight = 0
+        self.max_weight = max_weight
+        self.__things = []
 
-bag = Bag(max_weight)
-где max_weight - максимальный суммарный вес вещей,
-который выдерживает рюкзак (целое число).
+    @property
+    def things(self):
+        return self.__things
 
-В каждом объекте этого класса должен создаваться локальный приватный атрибут:
+    def add_thing(self, thing):
+        if self.max_weight - thing.weight >= 0:
+            self.__things.append(thing)
+            self.max_weight -= thing.weight
+            self.total_weight += thing.weight
 
-__things - список вещей в рюкзаке (изначально список пуст).
+    def remove_thing(self, indx):
+        x = len(self.things)
+        if x == indx + 1 or x > indx + 1:
+            self.max_weight += self.things[indx].weight
+            self.total_weight -= self.things[indx].weight
+            self.__things.pop(indx)
 
-Сам же класс Bag должен иметь объект-свойство:
+    def get_total_weight(self):
+        return self.total_weight
 
-things - для доступа к локальному приватному атрибуту __things (только для считывания, не записи).
 
-Также в классе Bag должны быть реализованы следующие методы:
+class Thing:
+    def __init__(self, name, weight):
+        self.name = name
+        self.weight = weight
 
-add_thing(self, thing) - добавление нового предмета в рюкзак
-(добавление возможно, если суммарный вес (max_weight) не будет превышен, иначе добавление не происходит);
-remove_thing(self, indx) - удаление предмета по индексу списка __things;
-get_total_weight(self) - возвращает суммарный вес предметов в рюкзаке.
 
-Каждая вещь описывается как объект класса Thing и создается командой:
-
-t = Thing(название, вес)
-где название - наименование предмета (строка);
-вес - вес предмета (целое или вещественное число).
-
-В каждом объекте класса Thing должны формироваться локальные атрибуты:
-
-name - наименование предмета;
-weight - вес предмета.'''
+bag = Bag(1000)
+bag.add_thing(Thing("Книга по Python", 100))
+bag.add_thing(Thing("Котелок", 500))
+bag.add_thing(Thing("Спички", 20))
+bag.add_thing(Thing("Бумага", 100))
+w = bag.get_total_weight()
+for t in bag.things:
+    print(f"{t.name}: {t.weight}")
