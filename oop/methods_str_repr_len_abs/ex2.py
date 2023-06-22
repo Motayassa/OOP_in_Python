30ad2731102022815c16cd7aa4bc66ff171a65bc
Motayassa <malovilyajob@gmail.com>
2023-06-22 16:59:31 +0300

    Finish ex2.py

--- oop/methods_str_repr_len_abs/ex2.py
+++ oop/methods_str_repr_len_abs/ex2.py
@@ -1,17 +1,19 @@
-'''Объявите класс с именем Model, объекты которого создаются командой:
+class Model:
+    def __init__(self):
+        self.count = None
+
+    def query(self, **kwargs):
+        self.count = kwargs
+
+    def __str__(self):
+        if self.count is None:
+            return 'Model'
+        else:
+            return 'Model: ' + ', '.join([f'{key} = {value}'
+                                          for key, value
+                                          in self.count.items()])
+
+
 model = Model()
-Объявите в этом классе метод query() для формирования записи базы данных. 
-Использоваться этот метод должен следующим образом:
-model.query(field_1=value_1, field_2=value_2, ..., field_N=value_N)
-Например:
 model.query(id=1, fio='Sergey', old=33)
-Все эти переданные данные должны сохраняться внутри объекта model класса Model.
-Затем, при выполнении команды:
 print(model)
-В консоль должна выводиться информация об объекте в формате:
-"Model: field_1 = value_1, field_2 = value_2, ..., field_N = value_N"
-Например:
-"Model: id = 1, fio = Sergey, old = 33"
-Если метод query() не вызывался, то в консоль выводится строка:
-"Model"
-P.S. В программе нужно только объявить класс, выводить в консоль ничего не нужно.'''
