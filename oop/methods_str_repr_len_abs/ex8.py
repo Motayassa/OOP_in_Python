ce2e244ed219b6a6e9256321e9074a31bc34f6dc
Motayassa <malovilyajob@gmail.com>
2023-06-22 21:09:28 +0300

    Finish ex8.py

--- oop/methods_str_repr_len_abs/ex8.py
+++ oop/methods_str_repr_len_abs/ex8.py
@@ -1,33 +1,26 @@
-'''Объявите класс Recipe для представления рецептов.
-Отдельные ингредиенты рецепта должны определяться классом Ingredient.
-Объекты этих классов должны создаваться командами:
-ing = Ingredient(name, volume, measure)
-recipe = Recipe()
-recipe = Recipe(ing_1, ing_2,..., ing_N)
-где ing_1, ing_2,..., ing_N - объекты класса Ingredient.
+class Recipe:
+    def __init__(self, *args):
+        self.recipe = list(args)
 
-В каждом объекте класса Ingredient должны создаваться локальные атрибуты:
+    def add_ingredient(self, ing):
+        self.recipe.append(ing)
 
-name - название ингредиента (строка);
-volume - объем ингредиента в рецепте (вещественное число);
-measure - единица измерения объема ингредиента (строка),
-например, литр, чайная ложка, грамм, штук и т.д.;
+    def remove_ingredient(self, ing):
+        if ing in self.recipe:
+            self.recipe.remove(ing)
 
-С объектами класса Ingredient должна работать функция:
+    def get_ingredients(self):
+        return tuple(self.recipe)
 
-str(ing)  # название: объем, ед. изм.
-и возвращать строковое представление объекта в формате:
-"название: объем, ед. изм."
-Например:
+    def __len__(self):
+        return len(self.recipe)
 
-ing = Ingredient("Соль", 1, "столовая ложка")
-s = str(ing) # Соль: 1, столовая ложка
-Класс Recipe должен иметь следующие методы:
 
-add_ingredient(ing) - добавление нового ингредиента ing (объект класса Ingredient) в рецепт (в конец);
-remove_ingredient(ing) - удаление ингредиента по объекту ing (объект класса Ingredient) из рецепта;
-get_ingredients() - получение кортежа из объектов класса Ingredient текущего рецепта.
+class Ingredient:
+    def __init__(self, name: str, volume: float, measure: str):
+        self.name = name
+        self.volume = volume
+        self.measure = measure
 
-Также с объектами класса Recipe должна поддерживаться функция:
-
-len(recipe) - возвращает число ингредиентов в рецепте.'''
+    def __str__(self):
+        return f'{self.name}: {self.volume}, {self.measure}'
