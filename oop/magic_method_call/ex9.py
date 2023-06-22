a859d93949bff372a789f97f2aea8abb0d5a3590
Motayassa <malovilyajob@gmail.com>
2023-06-22 13:03:10 +0300

    Finish ex9.py

---
+++ oop/magic_method_call/ex9.py
@@ -0,0 +1,22 @@
+class InputValues:
+    def __init__(self, render):     # render - ссылка на функцию или объект для преобразования
+        self.__render = render
+
+    def __call__(self, func, *args, **kwargs):     # func - ссылка на декорируемую функцию
+        def wrapper(*args, **kwargs):
+            return list(map(self.__render, func(*args, **kwargs).split()))
+        return wrapper
+
+
+class RenderDigit:
+    def __call__(self, string, *args, **kwargs):
+        try:
+            return int(string)
+        except:
+            return None
+
+
+render = RenderDigit()
+input_dg = InputValues(render)(input)
+res = input_dg()
+print(res)
