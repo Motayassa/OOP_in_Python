dc4c580ea07f4a17c1f9f9882be175335c57aed6
Motayassa <malovilyajob@gmail.com>
2023-06-25 19:34:38 +0300

    Finish ex6.py

--- oop/methods_add_sub_mul_div/ex6.py
+++ oop/methods_add_sub_mul_div/ex6.py
@@ -1,10 +1,45 @@
-'''Объявите класс Box3D для представления прямоугольного параллелепипеда (бруска),
-объекты которого создаются командой:
-box = Box3D(width, height, depth)
-где width, height, depth - ширина, высота и глубина соответственно (числа: целые или вещественные)
-В каждом объекте класса Box3D должны создаваться публичные атрибуты:
-width, height, depth - ширина, высота и глубина соответственно.
-С объектами класса Box3D должны выполняться следующие операторы:
+class Box3D:
+    def __init__(self, width, height, depth):
+        self.width = width
+        self.height = height
+        self.depth = depth
+
+    def __add__(self, other):
+        if type(other) == self.__class__:
+            return self.__class__(self.width + other.width,
+                                  self.height + other.height,
+                                  self.depth + other.depth)
+
+    def __rmul__(self, other):
+        if type(other) in (int, float):
+            return self.__class__(self.width * other,
+                                  self.height * other,
+                                  self.depth * other)
+
+    def __mul__(self, other):
+        if type(other) in (int, float):
+            return self.__class__(other * self.width,
+                                  other * self.height,
+                                  other * self.depth)
+
+    def __sub__(self, other):
+        if type(other) == self.__class__:
+            return self.__class__(self.width - other.width,
+                                  self.height - other.height,
+                                  self.depth - other.depth)
+
+    def __floordiv__(self, other):
+        if type(other) in (int, float):
+            return self.__class__(self.width // other,
+                                  self.height // other,
+                                  self.depth // other)
+
+    def __mod__(self, other):
+        if type(other) in (int, float):
+            return self.__class__(self.width % other,
+                                  self.height % other,
+                                  self.depth % other)
+
 box1 = Box3D(1, 2, 3)
 box2 = Box3D(2, 4, 6)
 box = box1 + box2 # Box3D: width=3, height=6, depth=9 (соответствующие размерности складываются)
@@ -13,5 +48,3 @@ box = 3 * box2    # Box3D: width=6, height=12, depth=18
 box = box2 - box1 # Box3D: width=1, height=2, depth=3 (соответствующие размерности вычитаются)
 box = box1 // 2   # Box3D: width=0, height=1, depth=1 (соответствующие размерности целочисленно делятся на 2)
 box = box2 % 3    # Box3D: width=2, height=1, depth=0
-При каждой арифметической операции следует создавать новый объект
-класса Box3D с соответствующими значениями локальных атрибутов.'''
