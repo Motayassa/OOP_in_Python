2d9012b24da7b85fecbc91a82329ec0603256537
Motayassa <malovilyajob@gmail.com>
2023-06-27 18:56:58 +0300

    Add ex5.py

--- oop/methods_eq_hash/ex5.py
+++ oop/methods_eq_hash/ex5.py
@@ -1,20 +1,40 @@
-'''Объявите класс с именем Dimensions, объекты которого создаются командой:
-d = Dimensions(a, b, c)
-где a, b, c - положительные числа (целые или вещественные),
-описывающие габариты некоторого тела: высота, ширина и глубина.
+class Dimensions:
+    def __init__(self, a, b, c):
+        if '.' in a:
+            self.a = float(a)
+            self.check(self.a)
+        else:
+            self.a = int(a)
+            self.check(self.a)
+        if '.' in b:
+            self.b = float(b)
+            self.check(self.b)
+        else:
+            self.b = int(b)
+            self.check(self.b)
+        if '.' in c:
+            self.c = float(c)
+            self.check(self.c)
+        else:
+            self.c = int(c)
+            self.check(self.c)
 
-Каждый объект класса Dimensions должен иметь аналогичные публичные
-атрибуты a, b, c (с соответствующими числовыми значениями). Также
-для каждого объекта должен вычисляться хэш на основе всех трех габаритов: a, b, c.
-С помощью функции input() прочитайте из входного потока строку, записанную в формате:
-"a1 b1 c1; a2 b2 c2; ... ;aN bN cN"
-Например:
-"1 2 3; 4 5 6.78; 1 2 3; 0 1 2.5"
-Если какой-либо габарит оказывается отрицательным значением или равен нулю,
-то при создании объектов должна генерироваться ошибка командой:
+    def __hash__(self):
+        return hash((self.a, self.b, self.c))
 
-raise ValueError("габаритные размеры должны быть положительными числами")
-Сформируйте на основе прочитанной строки список lst_dims из объектов класса
-Dimensions. После этого отсортируйте этот список по возрастанию (неубыванию)
-хэшей этих объектов так, чтобы объекты с равными хэшами стояли друг за другом.
-'''
+    def check(self, value):
+        if value <= 0:
+            raise ValueError("габаритные размеры должны быть положительными числами")
+        return True
+
+
+lst_dims = []
+x = "1 2 3; 4 5 6.78; 1 2 3; 0 1 2.5"
+lst = [x.split() for x in list(map(str.strip, x.split(';')))]
+print(lst)
+for i in lst:
+    a, b, c = i[0], i[1], i[2]
+    x = Dimensions(a, b, c)
+    lst_dims.append(x)
+lst_dims.sorted(key=hash)
+print(lst_dims)
