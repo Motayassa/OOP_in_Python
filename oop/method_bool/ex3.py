4c922eb5bd36e5d00a288bf9854af6198bc0626c
Motayassa <malovilyajob@gmail.com>
2023-06-28 15:55:54 +0300

    Finish ex3.py

---
+++ oop/method_bool/ex3.py
@@ -0,0 +1,12 @@
+from math import hypot
+
+
+class Line:
+    def __init__(self, x1, y1, x2, y2):
+        self.x1 = x1
+        self.x2 = x2
+        self.y1 = y1
+        self.y2 = y2
+
+    def __len__(self):
+        return (hypot(self.x2 - self.x1, self.y2 - self.y1)) > 1
